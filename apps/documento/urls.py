b4d30c74d86975916e65cc013b30a91417f74fa5
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2024-10-28 15:35:35 -0400

    up

--- apps/documento/urls.py
+++ apps/documento/urls.py
@@ -1,13 +1,12 @@
-from django import views
 from django.urls import path
-from .views import UploadDocumento
+from .views import UploadDocumento, DocumentoDelete
 
 urlpatterns = [
-    
 ]
 
 htmx_ulrpatterns = [
-    path('upload/<int:pk>/', UploadDocumento.as_view(), name='upload-documento')
+    path('upload/<int:pk>/', UploadDocumento.as_view(), name='upload-documento'),
+    path('delete/<int:pk>', DocumentoDelete.as_view(), name='delete-documento')
 ]
 
 urlpatterns += htmx_ulrpatterns
--- apps/documento/views.py
+++ apps/documento/views.py
@@ -2,6 +2,7 @@ from typing import Any
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import View
+from django.views.generic.edit import DeleteView
 from .models import Documento
 from apps.aluno.models import Aluno
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -23,3 +24,9 @@ class UploadDocumento(LoginRequiredMixin, View):
         except Exception as e:
             print(f'UploadDocumento -> {e}')
 
+class DocumentoDelete(LoginRequiredMixin, DeleteView):
+    model = Documento
+
+    def get_success_url(self):
+        documento = self.model.objects.get(id=self.kwargs.get('pk'))
+        return reverse('aluno-detail', args=[documento.aluno_id])
