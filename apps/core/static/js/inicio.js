81cceadfdfc1135c41225551d2e9df37381fe2bd
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2024-10-31 08:43:37 -0400

    Implementei modal de delete para lista de alunos

--- apps/core/static/js/inicio.js
+++ apps/core/static/js/inicio.js
@@ -1,21 +1,21 @@
    
-// // Modal de Delete
-// const modal = document.getElementById("modalDelete");
-// const openModalButton = document.getElementById("openModal");
-// const closeModalButton = document.querySelector(".close");
-// const deleteButton = document.getElementById("btn-excluir");
-// const formDelete = document.getElementById("formDelete");
+// Modal de Delete
+const modal = document.getElementById("modalDelete");
+const openModalButton = document.getElementById("openModal");
+const closeModalButton = document.querySelector(".close");
+const deleteButton = document.getElementById("btn-excluir");
+const formDelete = document.getElementById("formDelete");
 
-// openModalButton.onclick = function() {
-//     modal.style.display = "block";
-// };
+openModalButton.onclick = function() {
+    modal.style.display = "block";
+};
 
-// closeModalButton.onclick = function() {
-//     modal.style.display = "none";
-// };
+closeModalButton.onclick = function() {
+    modal.style.display = "none";
+};
 
-// deleteButton.onclick = function () {
-//     formDelete.submit();
-// };
+deleteButton.onclick = function () {
+    formDelete.submit();
+};
 
     
